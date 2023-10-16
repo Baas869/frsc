0ba1a307cf958087bc506ff7ce6ce73669fc7ca3
Baas869 <113592891+Baas869@users.noreply.github.com>
2023-10-15 17:45:25 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let btn = document.getElementsByClassName('btn-register').style.background = 'black';
+
+document.getElementById('btn-register').style.background = '#333'; 
+
+document.getElementsByClassName('btn-register').style.background = '#333';
